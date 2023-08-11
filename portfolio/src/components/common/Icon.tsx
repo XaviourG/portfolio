0c61d00e95c2ecefd7eb2a3a260aaf0010595334
Xaviour Greenhalgh <xaviour@squareiq.com.au>
2023-08-11 22:09:43 +1000

    progress on contact section

---
+++ portfolio/src/components/common/Icon.tsx
@@ -0,0 +1,55 @@
+import { ReactElement } from "react";
+
+import InstagramIcon from "@/assets/instagram_icon";
+
+export interface IconProps {
+  icon: string;
+  color?: string;
+  size?: string;
+}
+
+/**
+ * Adding an editable SVG to CustomIconV2:
+  ---------------------------------------------------
+  1. Add XXX-icon.svg to assets/icons/custom_svg
+
+  2. rename to XXX-icon.tsx and inject basic component stuff (const XXXIcon = () =>), (export default XXXIcon;)
+
+  3a. Inside XXX-icon.tsx replace all fill or stroke arguments with "currentColor"
+
+  3b. Inside XXX-icon.tsx replace width and height with ratio'd width and height in percentage, where the larger is 100% (ex: width=15,  height=16    ==>    width=87%,  height=100%)
+
+  3c. If present in XXX-icon.tsx, replace "clip-rule" with "clipRule" and "fill-rule" with "fillRule"
+
+  4. Import XXXIcon into CustomIconV2
+  -------------------------------------------------- 
+ */
+const Icon = ({
+  icon,
+  color = 'inherit',
+  size = '100%',
+}: IconProps): ReactElement => {
+  const icons = {
+    InstagramIcon,
+  };
+
+  const Icon = icons[icon as keyof typeof icons]
+    ? icons[icon as keyof typeof icons]
+    : icons.InstagramIcon;
+
+  return (
+    <div style={{
+      width: size,
+      height: size,
+      color: color,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+    >
+      <Icon/>
+    </div>
+  );
+};
+
+export default Icon;
--- portfolio/src/panels/ContactSegment.tsx
+++ portfolio/src/panels/ContactSegment.tsx
@@ -1,9 +1,27 @@
+import InstagramIcon from "@/assets/instagram_icon";
+import Icon from "@/components/common/Icon";
 import { ReactElement } from "react";
 
 const ContactSegment = (): ReactElement => {
   return <div className='flex flex-col w-full p-32 justify-center items-center'>
-    <p>xaviourgreenhalgh@gmail.com</p>
-    <p>+61 416 612 608</p>
+
+  <div className="h-24 w-full rounded-s-full flex items-center p-0 m-0
+    border-solid border-s-2 border-b-2 border-e-0 border-t-0 border-SECONDARY">
+    <div className='w-24 h-24 rounded-full p-0 m-0'>
+      <img
+        className='rounded-full p-0 m-0'
+        src="https://drive.google.com/uc?export=view&id=1T_Qase6tdBwsq1sysHfNrGp3RAt-0w2P"
+        alt="pfp"
+      />
+    </div>
+    <div className='flex w-full justify-around text-PRIMARY p-0 m-0'>
+      <Icon icon='LinkedinIcon' color='inherit' size='40px'></Icon>
+      <Icon icon='InstagramIcon' color='inherit' size='40px'></Icon>
+      <Icon icon='EmailIcon' color='inherit' size='40px'></Icon>
+      <Icon icon='PhoneIcon' color='inherit' size='40px'></Icon>
+    </div>
+  </div>
+
     <p>https://linkedin.com/in/xaviourg</p>
     <p>https://github.com/xaviourg</p>
     <p>https://instagram.com/xaviourg</p>
