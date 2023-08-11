7ba93bd3ed6d89e15836bff789f02daa2b77f7a0
Xaviour Greenhalgh <xaviour@squareiq.com.au>
2023-08-11 18:32:23 +1000

    update structure with menu

--- portfolio/src/app/portfolioBody.tsx
+++ portfolio/src/app/portfolioBody.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import NavigationBar from "@/components/top_bar/NavigationBar";
 import AboutSegment from "@/panels/AboutSegment";
 import ContactSegment from "@/panels/ContactSegment";
 import ExperienceSegment from "@/panels/ExperienceSegment";
@@ -7,7 +8,10 @@ import ProjectsSegment from "@/panels/ProjectsSegment";
 import { ReactElement } from "react"
 
 const PortfolioBody = (): ReactElement => {
-  return <div className='flex w-full'>
+  return <div className='flex w-full flex-col'>
+    <div className='sm:flex xl:hidden w-full sticky top-0'>
+      <NavigationBar/>
+    </div>
     <div className='flex w-full sm:flex-col xl:flex-row'>
       <div className='flex flex-col sm:w-full xl:w-1/2'>
         <AboutSegment/>
--- portfolio/src/panels/AboutSegment.tsx
+++ portfolio/src/panels/AboutSegment.tsx
@@ -4,7 +4,15 @@ const AboutSegment = (): ReactElement => {
   return <div className='flex flex-col bg-blue-600 justify-center items-center
     xl:fixed xl:w-1/2 x: h-screen
     sm:static sm:w-full sm:h-full'>
-    ABOUT
+    <div>
+      <h1>Xaviour Greenhalgh</h1>
+      <h2>Software Engineer</h2>
+
+      {/* MENU */}
+      <div className='sm:hidden xl:flex w-full'>
+        HIDDEN MENU BABY
+      </div>
+    </div>
   </div>
 }
 
