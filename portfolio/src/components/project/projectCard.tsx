9eac29f49c9bb6fe6c6139e56305fb5f1f442179
Xaviour Greenhalgh <xaviourgreenhalgh@gmail.com>
2023-08-12 14:28:30 +1000

    project desktop sizing

--- portfolio/src/components/project/projectCard.tsx
+++ portfolio/src/components/project/projectCard.tsx
@@ -17,7 +17,7 @@ const ProjectCard = ({
   return <div className='flex flex-col xl:flex-row p-8 m-8 text-inherit filter saturate-50 hover:saturate-100
   hover:text-NEUTRAL_LIGHT
   duration-100'>
-    <div className="w-full xl:w-1/4 max-h-32 flex justify-center pl-1 pr-2">
+    <div className="w-full xl:w-1/3 max-h-32 flex justify-center pl-1 pr-2">
       <div className="w-full border-solid border-2 border-NEUTRAL_DARK bg-NEUTRAL_DARK rounded-2xl">
         <img
           className="w-full h-full object-cover rounded-2xl"
@@ -26,7 +26,7 @@ const ProjectCard = ({
         />
       </div>
     </div>
-    <div className="w-full xl:w-3/4 pt-3 xl:pt-0">
+    <div className="w-full xl:w-2/3 pt-3 xl:pt-0">
       <h3 className="text-inherit">{title}</h3>
       <p className="text-inherit pt-1 pb-1">{description}</p>
       <TechTags tags={tech}/>
