f23dbb78c305320087cdeab817ac5574661cea1c
Xaviour Greenhalgh <xaviourgreenhalgh@gmail.com>
2023-08-14 15:44:31 +1000

    portfolio content

--- portfolio/src/content/ProjectsContent.ts
+++ portfolio/src/content/ProjectsContent.ts
@@ -1,5 +1,6 @@
 export interface ProjectContent {
   title: string,
+  subtitle: string,
   image: string,
   tech: string[],
   description: string,
@@ -7,22 +8,32 @@ export interface ProjectContent {
 
 const ProjectsContent: ProjectContent[] = [
   {
-    title: 'Organelle | Digital Pathology',
+    title: 'Organelle',
+    subtitle: 'Digital Pathology Web App',
     image: 'https://e1.pxfuel.com/desktop-wallpaper/280/759/desktop-wallpaper-funniest-group-weird-stuff-thumbnail.jpg',
     tech: ['NodeJS', 'TypeScript', 'React', 'AWS', 'Docker', 'HCP', 'Python', 'HL7', 'Stripe', 'Jest', 'MySQL'],
-    description: "This is a generic description for a template project. I'm guessing this is long enough. But who knows maybe it should be even longer, maybe it should be incredibly stupendously incorduously long.",
+    description: "Architected, designed, developed, launched and maintained as team lead then later CEO of Organelle. Machine learning driven blood testing analysis, clinical data pipelines, telehealth consultations, full eCommerce capabilities, and dual practitioner/client interfaces.",
   },
   {
-    title: 'XaviourG.com | Web Portfolio',
+    title: 'XaviourG.com',
+    subtitle: 'Web Portfolio',
     image: 'https://e1.pxfuel.com/desktop-wallpaper/280/759/desktop-wallpaper-funniest-group-weird-stuff-thumbnail.jpg',
     tech: ['NextJS', 'TailwindCSS', 'ThreeJS', 'Typescript', 'React', 'Firebase'],
-    description: "This is a generic description for a template project. I'm guessing this is long enough. But who knows maybe it should be even longer, maybe it should be incredibly stupendously incorduously long.",
+    description: "A UX optimised showcase of my skills and experience built on the latest and greatest front-end technology.",
   },
   {
-    title: 'template project 3',
+    title: 'Simple Intentions',
+    subtitle: 'Native Android Task Management App',
     image: 'https://e1.pxfuel.com/desktop-wallpaper/280/759/desktop-wallpaper-funniest-group-weird-stuff-thumbnail.jpg',
-    tech: ['NodeJS', 'TypeScript', 'React'],
-    description: "This is a generic description for a template project. I'm guessing this is long enough. But who knows maybe it should be even longer, maybe it should be incredibly stupendously incorduously long.",
+    tech: ['Kotlin', 'Android SDK', 'Java'],
+    description: "Combining android state management, widgets, and notifications with functional UI to create self-published app that perfectly suites my productivity style.",
+  },
+  {
+    title: 'Ragnarok Racer',
+    subtitle: 'Exe Platforming Game',
+    image: 'https://e1.pxfuel.com/desktop-wallpaper/280/759/desktop-wallpaper-funniest-group-weird-stuff-thumbnail.jpg',
+    tech: ['Java', 'Sonarcloud', 'Javadoc'],
+    description: "A collaborative 28 person university project I led, it oversaw the design and development of a physics engine, NPC AI, sprite loader, and level builder.",
   },
 ]
 
