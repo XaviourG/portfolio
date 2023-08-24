c7a27b425604f3e6ae12dc899d1f12d8530657bb
Xaviour Greenhalgh <xaviour@squareiq.com.au>
2023-08-24 17:01:06 +1000

    Add header and subtext to skillset

--- portfolio/src/components/skillset/Skillset.tsx
+++ portfolio/src/components/skillset/Skillset.tsx
@@ -42,9 +42,10 @@ const Skillset = ({
   }
 
   return (
-    <div className="flex flex-col w-full items-center m-2 xk:m-16 mt-16 mb-16 rounded-2xl">
-      <p className="text-xl text-SECONDARY">Skillset</p>
+    <div className="flex flex-col w-full items-center m-2 mt-16 mb-16 xl:m-20  rounded-2xl">
+      <p className="text-2xl text-SECONDARY50 font-bold">Raw Technical Experience</p>
       <SkillCloud skills={skills}/>
+      <p className="text-lg text-SECONDARY50">click/hover for details</p>
     </div>
   );
 }
